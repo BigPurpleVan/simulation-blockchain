7ceae81804a6ed29861125ac7929828d414d3bd1
Evan Delbecq <106834827+BigPurpleVan@users.noreply.github.com>
2024-06-20 13:49:13 +0200

    feat: Clicking blocks to see transactions

--- src/Pages/Blocks.js
+++ src/Pages/Blocks.js
@@ -1,70 +1,67 @@
 import React from 'react'
 import {exchain} from '../App.js'
-import { Card, CardBody, CardFooter, CardHeader, Spacer} from '@nextui-org/react'
-
+import { Card, CardBody, CardFooter, CardHeader, Spacer, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from '@nextui-org/react'
 
 export default function Blocks() {
-  console.log(exchain.chain[1])
-    return (
-      <div>
-        <h2 className='text-xl font-bold m-3'>Blocks enregistrées dans la blockchain</h2>
-        <div className='flex'>
-            {exchain.chain.map((block, index) => {
-                return (
-                  <>
-                    <Card className='max-w-96 whitespace-nowrap cursor-pointer' key={index}>
-                        <CardHeader>
-                          <h3 className='text-lg font-bold'>Block {index}</h3>
-                        </CardHeader>
-                        <CardBody>
-                          <p className='overflow-hidden text-ellipsis'>Hash: {block.hash}</p>
-                          <p className='overflow-hidden text-ellipsis'>Previous Hash: {block.previousHash}</p>
-                        </CardBody>
-                        <CardFooter className='flex flex-col items-start'>
-                          <p>Date : {new Date(block.timestamp).toLocaleString()}</p>
-                          <p>Timestamp: {block.timestamp}</p>
-                          <p>Nonce: {block.nonce}</p>
-                        </CardFooter>
-                    </Card>
-                    <Spacer x={4} />
-                  </>
-                )
-            })}
-        </div>
-        <div>
-        {exchain.chain.transactions === undefined ? (<div className='hidden'> <p>Pas de transaction</p> </div>) : exchain.chain.transactions.map((transaction, index) => {
-          return transaction  ? (
-                  <div className='hidden' key={index}>
-                    <table>
-                      <tr>
-                        <th>Transaction {index}</th>
-                      </tr>
-                      <tr>
-                        <td>Source:</td>
-                        <td>{transaction.fromAddress}</td>
-                      </tr>
-                      <tr>
-                        <td>Destinataire:</td>
-                        <td>{transaction.toAddress}</td>
-                      </tr>
-                      <tr>
-                        <td>Montant:</td>
-                        <td>{transaction.amount}</td>
-                      </tr>
-                      <tr>
-                        <td>Date:</td>
-                        <td>{new Date(transaction.timestamp * 1000).toLocaleString()}</td>
-                      </tr>
-                      <tr>
-                        <td>Signature:</td>
-                        <td>{transaction.signature}</td>
-                      </tr>
-                    </table>
-                    <Spacer y={4} />
-                  </div>
-                ) : (<></>)
-            }) } 
-        </div>
+  const [infoTransaction, setInfoTransaction] = React.useState()
+  
+  
+  return (
+    <div>
+      <h2 className='text-xl font-bold m-3'>Blocks enregistrées dans la blockchain</h2>
+      <div className='flex'>
+          {exchain.chain.map((block, index) => (
+            <>
+              <Card isPressable={true} className='max-w-96 whitespace-nowrap cursor-pointer' key={index} onPress={()=>{setInfoTransaction(
+                block.transactions
+              )}}>
+                  <CardHeader>
+                    <h3 className='text-lg font-bold'>Block {index}</h3>
+                  </CardHeader>
+                  <CardBody>
+                    <p className='overflow-hidden text-ellipsis'>Hash: {block.hash}</p>
+                    <p className='overflow-hidden text-ellipsis'>Previous Hash: {block.previousHash}</p>
+                  </CardBody>
+                  <CardFooter className='flex flex-col items-start'>
+                    <p>Date : {new Date(block.timestamp*1000).toLocaleString()}</p>
+                    <p>Timestamp: {block.timestamp}</p>
+                    <p>Nonce: {block.nonce}</p>
+                  </CardFooter>
+              </Card>
+              <Spacer x={4} />
+            </>
+          ))}
       </div>
-    )
+        {infoTransaction === undefined  ? <div>Pas de transaction</div> : infoTransaction.map((transaction, index) => (
+          <Table key={index}>
+            <TableHeader>
+              <TableColumn>Transaction {index}</TableColumn>
+              <TableColumn></TableColumn>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>Source</TableCell>
+                <TableCell>{transaction.fromAddress}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Destination</TableCell>
+                <TableCell>{transaction.toAddress}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Montant</TableCell>
+                <TableCell>{transaction.amount}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell>{new Date(transaction.timestamp*1000).toLocaleString()}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Signature</TableCell>
+                <TableCell>{transaction.signature}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+            ))}
+    </div>
+  )
 }
